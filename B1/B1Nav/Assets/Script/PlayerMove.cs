5193ed984b862f175ed654afd2e50d1daa41ff11
DESKTOP-NLIJLIS\Tian <tianqi.nan@rutgers.edu>
2016-09-26 22:38:50 -0400

    First Upload

---
+++ B1/B1Nav/Assets/Script/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Animator Anime;
+    private NavMeshAgent Agent;
+    private Transform Target;
+    private bool Walking;
+
+	// Use this for initialization
+	void Awake () {
+        Anime = GetComponent<Animator>();
+        Agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                Walking = true;
+                Agent.destination = hit.point;
+                Agent.Resume();
+            }
+        }
+    }
+}
